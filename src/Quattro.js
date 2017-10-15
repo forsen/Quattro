471f100fe84a8fb5d53cd4c7ee7f27dd3f162ee8
Erik Haider Forsén <erik@forsen.no>
2017-10-15 11:34:25 +0200

    Persist redux store on app quit

--- src/Quattro.js
+++ src/Quattro.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from 'react'
+import { Provider } from 'react-redux'
 
-import Dummy from './containers/Landing';
-import configureStore from './store/index';
+import Dummy from './containers/Landing'
+import configureStore from './store/index'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Text, View } from 'react-native'
 
-const store = configureStore();
+const {store, persistor} = configureStore()
 
 const Quattro = () => (
   <Provider store={store}>
-    <Dummy />
+    <PersistGate
+      persistor={persistor}
+      loading={<View><Text>LOADING BITCH</Text></View>}
+    >
+      <Dummy />
+    </PersistGate>
   </Provider>
-);
+)
 
-export default Quattro;
+export default Quattro
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
+import { persistStore, persistReducer } from 'redux-persist';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/es/storage'
 import rootReducer from '../reducers';
 
+const config = {
+  key: 'root',
+  storage
+}
+
+const reducer = persistReducer(config, rootReducer)
 export default function configureStore() {
   const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+  let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+  let persistor = persistStore(store)
+  return { persistor, store }
 }
